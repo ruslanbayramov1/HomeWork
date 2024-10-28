48513462ffa934d348be17af83fb6d56e05f9180
ruslanbayramov1 <ruslanyb-ab108@code.edu.az>
2024-10-28 18:55:25 +0400

    exception tasks

---
+++ 08_Exception/Meeting.cs
@@ -0,0 +1,62 @@
+﻿namespace _08_Exception;
+
+class Meeting
+{
+    private DateTime _toDate;
+    public DateTime FromDate { get; set; }
+    public DateTime ToDate 
+    {
+        get => _toDate;
+        set
+        {
+            if (value <= FromDate)
+                throw new WrongDateIntervalException("To date can not be less than from date!");
+            else
+            { 
+                _toDate = value;
+            }
+        }
+    }
+    public string Name { get; set; }
+
+    public Meeting(string name, DateTime from, DateTime to)
+    {
+        Name = name;
+        FromDate = from;
+        ToDate = to;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.GetType().ToString().Split('.')[^1]} {{ Name: {Name}, From: {FromDate.Date}, To: {ToDate.Date} }}";
+    }
+}
+
+class MeetingSchedule
+{
+    private Meeting[] _meetings = new Meeting[0];
+    public Meeting[] Meetings { get => _meetings; } 
+
+    public void SetMeeting(string name, DateTime from, DateTime to)
+    {
+        foreach (Meeting meeting in Meetings)
+        {
+            if (from >= meeting.FromDate && meeting.ToDate >= from)
+                throw new ReservedDateIntervalException($"We already have meeting for this time: {{ Name: {meeting.Name}, From: {meeting.FromDate.Date}, To: {meeting.ToDate.Date} }}");
+        }
+
+        Meeting newMeeting = new(name, from, to);
+        Array.Resize(ref _meetings, _meetings.Length + 1);
+        _meetings[_meetings.Length - 1] = newMeeting;
+    }
+}
+
+class ReservedDateIntervalException : Exception
+{
+    public ReservedDateIntervalException(string message) : base(message) { }
+}
+
+class WrongDateIntervalException : Exception
+{
+    public WrongDateIntervalException(string message) : base(message) { }
+}
---
+++ 08_Exception/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _08_Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MeetingSchedule mainSchedule = new();
+                mainSchedule.SetMeeting("Oct. Meeting", new DateTime(2024, 08, 14), new DateTime(2024, 08, 25));
+                mainSchedule.SetMeeting("Sep. Meeting", new DateTime(2024, 09, 15), new DateTime(2024, 09, 20));
+                Console.WriteLine(mainSchedule.Meetings[0]);
+                Console.WriteLine(mainSchedule.Meetings[1]);
+
+                mainSchedule.SetMeeting("Sep. Second Meeting", new DateTime(2024, 09, 20), new DateTime(2024, 09, 25)); // reserved date error
+                mainSchedule.SetMeeting("Nov. Meeting", new DateTime(2024, 11, 14), new DateTime(2024, 11, 13)); // wrong date error
+            }
+            catch (ReservedDateIntervalException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (WrongDateIntervalException ex)
+            {
+                Console.WriteLine($"Wrong date interval: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Reservations finished!");
+            }
+        }
+    }
+}
