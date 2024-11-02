c263369e4991a4ca3f8785c408b9bb4f7e010ee1
ruslanbayramov1 <ruslanyb-ab108@code.edu.az>
2024-11-02 17:44:57 +0400

    json crud, with filestream async await

---
+++ 11_FileStream/FileHelper.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace _11_FileStream;
+
+static class FileHelper
+{
+    public static async Task<List<string>> ReadDataAsync(string path)
+    {
+        using StreamReader sr = new StreamReader(path);
+        string text = await sr.ReadToEndAsync();
+
+        List<string>? data = JsonConvert.DeserializeObject<List<string>>(text);
+        if (data == null)
+            return new List<string>();
+
+        return data;
+    }
+
+    public static async Task WriteDataAsync(string path, List<string> data)
+    {
+        using StreamWriter sw = new StreamWriter(path, false);
+        string text = JsonConvert.SerializeObject(data);
+        await sw.WriteAsync(text);
+    }
+}
---
+++ 11_FileStream/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _11_FileStream
+{
+    internal class Program
+    {
+        const string path = @"C:\AB108\home-tasks\11_FileStream\Data\names.json";
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (!File.Exists(path))
+                    File.Create(path).Close();
+
+                Add("Jack").Wait();
+                Add("Jim").Wait();
+                Add("Adam").Wait();
+
+                Task<bool> task2 = Exist("John");
+                task2.Wait();
+                Console.WriteLine(task2.Result);
+
+                Update(1, "Jimmie").Wait();
+                Delete(0).Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static async Task Add(string name)
+        {
+            List<string> names = await FileHelper.ReadDataAsync(path);
+            names.Add(name);
+            await FileHelper.WriteDataAsync(path, names);
+        }
+        static async Task<bool> Exist(string name)
+        {
+            List<string> names = await FileHelper.ReadDataAsync(path);
+            bool isExists = names.Exists(n => n == name);
+            return isExists;
+        }
+
+        static async Task Update(int index, string name)
+        {
+            List<string> names = await FileHelper.ReadDataAsync(path);
+            if (index > names.Count || index < 0)
+                throw new Exception("Index is big or less than bounds of list");
+
+            names[index] = name;
+            await FileHelper.WriteDataAsync(path, names);
+        }
+
+        static async Task Delete(int index)
+        {
+            List<string> names = await FileHelper.ReadDataAsync(path);
+            names.RemoveAt(index);
+            await FileHelper.WriteDataAsync(path, names);
+        }
+    }
+}
