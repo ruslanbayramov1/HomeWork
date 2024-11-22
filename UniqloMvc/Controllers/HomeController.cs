32b6bb86ed184eff90d7cb7d255768f9db2d8a31
ruslanbayramov1 <ruslanyb-ab108@code.edu.az>
2024-11-22 04:40:17 +0400

    creating slider admin panel with update, delete, hide, show, create, read

---
+++ UniqloMvc/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniqloMvc.DataAccess;
+using UniqloMvc.Models;
+
+namespace UniqloMvc.Controllers;
+
+public class HomeController(UniqloDbContext _context) : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        List<Slider> sliders = await _context.Sliders.Where(slider => slider.IsDeleted == false).ToListAsync();
+        return View(sliders);
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+}
---
+++ UniqloMvc/DataAccess/UniqloDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniqloMvc.Models;
+
+namespace UniqloMvc.DataAccess
+{
+    public class UniqloDbContext : DbContext
+    {
+        public DbSet<Slider> Sliders { get; set; }
+
+        public UniqloDbContext(DbContextOptions<UniqloDbContext> opt) : base(opt)
+        {
+            
+        }
+    }
+}
