d8677c5660d6443902c82f27daf3e33c7ba5f14d
ruslanbayramov1 <ruslanyb-ab108@code.edu.az>
2024-11-24 16:51:57 +0400

    fix

--- UniqloMvc/Models/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UniqloMvc.Models
-{
-    public abstract class BaseEntity
-    {
-        public int Id { get; set; }
-        public DateTime CreatedTime { get; set; }
-        public bool IsDeleted { get; set; }
-    }
-}
